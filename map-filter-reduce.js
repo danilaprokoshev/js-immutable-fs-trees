9863f634a964a95be2abf1ee513995d78ddcbfa0
Danila Prokoshev <danilaprokoshev@gmail.com>
2020-12-14 20:38:39 +0300

    updated map-filter-reduce

--- map-filter-reduce.js
+++ map-filter-reduce.js
@@ -29,23 +29,36 @@ const tree = mkdir('/', [
 const tree2 = mkfile('smth');
 
 const filter = (fn, node) => {
-  let result = {};
+  let result = [];
     if (fn(node)) {
-        //let result;
         result = { ...node };
-        // if (isDirectory(node)) {
-        //   const children = getChildren(node);
-        //   const newChildren = [];
-        //   for (const child of children) {
-        //     newChildren.push(filter(fn, child));
-        //   }
-        //   result['children'] = newChildren;
-        // }
-        return result;
+        if (isDirectory(node)) {
+          const children = getChildren(node);
+          const newChildren = [];
+          for (const child of children) {
+            newChildren.push(filter(fn, child));
+          }
+          result['children'] = newChildren.flat();
+        }
     }
-    return;
 
+    return result;
 };
 
-const isDirect = (n) => n.type === 'directory';
-console.log(filter(isDirect, tree2));
+const reduce = (fn, node, startAcc = 0) => {
+    let current = startAcc;
+    current = fn(startAcc, node);
+    console.log(node);
+    if (isDirectory(node)) {
+        const children = getChildren(node);
+        for (const child of children) {
+            current = reduce(fn, child, current);
+            console.log(current);
+        }
+    }
+
+    return current;
+};
+
+const counterNodes = (acc) => acc + 1;
+console.log(reduce(counterNodes, tree, 0));
